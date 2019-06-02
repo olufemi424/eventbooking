b9efecaf50b2fa129baf349e139a05bdfa262340
Olufemi Afolabi <femiafolabidiipo@gmail.com>
2019-06-01 20:49:52 -0500

    sending and requesting data

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const { buildSchema } = require("graphql");
 //set up express
 const app = express();
 
+const events = [];
+
 //body parser
 app.use(bodyParser.json());
 
@@ -14,12 +16,27 @@ app.use(
   "/graphql",
   graphqlHttp({
     schema: buildSchema(`
+      type Event {
+         _id: ID!
+         title: String!
+         description: String!
+         price: Float!
+         date: String!
+      }
+
+      input EventInput {
+         title: String!
+         description: String!
+         price: Float!
+         date: String!
+      }
+
       type RootQuery {
-         events: [String!]!
+         events: [Event!]!
       }
 
       type RootMutation {
-         createEvent(name: String): String
+         createEvent(eventInput: EventInput): Event
       }
 
       schema {
@@ -29,11 +46,19 @@ app.use(
     `),
     rootValue: {
       events: () => {
-        return ["Romantic", "Cooking", "Sailing", "All night coding"];
+        const rqEvents = [...events];
+        return rqEvents;
       },
       createEvent: args => {
-        const eventName = args.name;
-        return eventName;
+        const event = {
+          _id: Math.random().toString(),
+          title: args.eventInput.title,
+          description: args.eventInput.description,
+          price: +args.eventInput.price,
+          date: new Date().toISOString()
+        };
+        events.push(event);
+        return event;
       }
     },
     graphiql: true
