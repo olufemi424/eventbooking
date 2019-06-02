2b6378ed6aa03f981e2f75474a1f6ea3ae3ddb27
Olufemi Afolabi <femiafolabidiipo@gmail.com>
2019-06-01 20:17:31 -0500

    type, query, schema and rootValue

--- app.js
+++ app.js
@@ -1,14 +1,47 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const graphqlHttp = require("express-graphql");
+const { buildSchema } = require("graphql");
 
 //set up express
 const app = express();
+
+//body parser
 app.use(bodyParser.json());
 
-app.get("/", (req, res, next) => {
-  res.send("Hello World");
-});
+//handling query being sent using the grapghql pkg
+app.use(
+  "/graphql",
+  graphqlHttp({
+    schema: buildSchema(`
+      type RootQuery {
+         events: [String!]!
+      }
+
+      type RootMutation {
+         createEvent(name: String): String
+      }
+
+      schema {
+         query: RootQuery
+         mutation: RootMutation
+      }
+    `),
+    rootValue: {
+      events: () => {
+        return ["Romantic", "Cooking", "Sailing", "All night coding"];
+      },
+      createEvent: args => {
+        const eventName = args.name;
+        return eventName;
+      }
+    },
+    graphiql: true
+  })
+);
 
 //set up server
 const PORT = 3000;
-app.listen(PORT, () => console.log("Server running on " + PORT));
+app.listen(PORT, () => {
+  console.log(`localhost:${PORT}/graphql`);
+});
