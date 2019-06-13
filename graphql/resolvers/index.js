4b21b55b7a9da845fac3a0bf558e8f6ba20782c2
Olufemi Afolabi <femiafolabidiipo@gmail.com>
2019-06-12 22:42:18 -0400

    bookings, single event error

--- graphql/resolvers/index.js
+++ graphql/resolvers/index.js
@@ -1,6 +1,7 @@
 //models
 const Event = require("../../models/event");
 const User = require("../../models/user");
+const Booking = require("../../models/booking");
 const bcrypt = require("bcryptjs");
 
 const events = async eventIds => {
@@ -14,6 +15,19 @@ const events = async eventIds => {
         creator: user.bind(this, event.creator)
       };
     });
+  } catch (err) {
+    throw err;
+  }
+};
+
+const singleEvent = async eventId => {
+  try {
+    const event = Event.findById(eventId);
+    return {
+      ...event._doc,
+      _id: event.id,
+      creator: user.bind(this, event.creator)
+    };
   } catch (err) {
     throw err;
   }
@@ -48,6 +62,25 @@ module.exports = {
       throw err;
     }
   },
+
+  bookings: async () => {
+    try {
+      const bookings = await Booking.find({});
+      return bookings.map(booking => {
+        return {
+          ...booking._doc,
+          _id: booking.id,
+          user: user.bind(this, booking._doc.user),
+          event: singleEvent.bind(this, booking._doc.event),
+          createdAt: new Date(booking._doc.createdAt).toISOString(),
+          updatedAt: new Date(booking._doc.updatedAt).toISOString()
+        };
+      });
+    } catch (err) {
+      throw err;
+    }
+  },
+
   createEvent: async args => {
     try {
       const event = new Event({
@@ -96,5 +129,23 @@ module.exports = {
     } catch (err) {
       throw err;
     }
+  },
+
+  bookEvent: async args => {
+    const fetchedEvent = await Event.findOne({ _id: args.eventId });
+    const booking = new Booking({
+      user: "5cf33f8b352cda73383c8f20",
+      event: fetchedEvent
+    });
+
+    const result = await booking.save();
+    return {
+      ...result._doc,
+      _id: result.id,
+      user: user.bind(this, booking._doc.user),
+      event: singleEvent.bind(this, booking._doc.event),
+      createdAt: new Date(result._doc.createdAt).toISOString(),
+      updatedAt: new Date(result._doc.updatedAt).toISOString()
+    };
   }
 };
